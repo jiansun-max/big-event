1d935584530c8e8586161a364f389d34eb097645
Hui <286727179@qq.com>
2023-11-09 19:53:39 +0800

    first commite

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <VC></VC>
+</template>
+
+<script setup>
+import VC from './components/VC.vue'
+</script>
+
+<style scoped></style>
---
+++ src/components/VC.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>{{ titel }}</div>
+</template>
+
+<script setup>
+import {} from 'vue'
+let titel = 'Hello Vue'
+</script>
+
+<style scoped></style>
